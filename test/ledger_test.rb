b2d2bae31a65b6a4330b7150f0309fce2a5c0788
Rob <rold@fleetsmart.co.uk>
2020-02-24 17:23:50 +0000

    Wrote a failing test for failing contract verifications. Before trying to make it pass, need to refactor Spec implementation.

--- test/ledger_test.rb
+++ test/ledger_test.rb
@@ -4,21 +4,14 @@ require_relative './dumb_object'
 class LedgerTest < MiniTest::Test
 
   def test_spec_verification_in_order
-    ledger = Ledger.new
+    ledger = ledger_with_contract
     collaborator = DumbObject.new
-    stub = TestHelpers.stubs_add_one_two
-    ledger.record_contract('adder', stub)
-    # This would occur inside some method under test
-    stub.add(1,2)
     ledger.verify_contract('adder', collaborator){|collaborator| collaborator.add(1,2) }
     assert_equal 'All test double contracts are satisfied.', ledger.summary
   end
 
   def test_unverified_spec
-    ledger = Ledger.new
-    stub = TestHelpers.stubs_add_one_two
-    ledger.record_contract('adder', stub)
-    stub.add(1,2)
+    ledger = ledger_with_contract
     expected = <<~MSG
       The following contracts could not be verified:
       Role Name: adder
@@ -47,5 +40,40 @@ class LedgerTest < MiniTest::Test
       ================================================================================
     MSG
     assert_equal expected, ledger.summary
+  end
+
+  def test_failing_spec
+    skip
+    ledger = Ledger.new
+
+    bad_collaborator = Object.new
+    def bad_collaborator.add(x,y)
+      -1
+    end
+    ledger.verify_contract('adder', bad_collaborator){|adder| adder.add(1,2) }
+    expected = <<~MSG
+      The following contracts could not be verified:
+      Role Name: adder
+      Attempts to verify the following method invocations failed:
+      ================================================================================
+      method: add
+      invoke with: [1, 2]
+      expected: 3
+      returned: -1
+      ================================================================================
+    MSG
+    assert_equal expected, ledger.summary
+  end
+
+  private
+
+  def ledger_with_contract
+    ledger = Ledger.new
+    stub = TestHelpers.stubs_add_one_two
+    ledger.record_contract('adder', stub)
+    # in real useage stub would be injected as a dependency, and this would be
+    # invoked in a collaboration test
+    stub.add(1, 2)
+    ledger
   end
 end

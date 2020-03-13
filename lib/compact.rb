b22715700737a0c8270a4cb680e2d0bd722d3129
Rob <rold@fleetsmart.co.uk>
2020-03-13 22:52:10 +0000

    Decided the ledgerWrapper class was a pointless layer of indirection.

--- lib/compact.rb
+++ lib/compact.rb
@@ -4,32 +4,18 @@ require 'compact/ledger'
 require 'compact/verification_codes'
 
 module Compact
-  class LedgerWrapper
-    @ledger = Ledger.new
-
-    def self.prepare_double(name, block = Proc.new)
-      @ledger.prepare_double(name, block)
-    end
-
-    def self.verify_contract(name, collaborator, block = Proc.new )
-      @ledger.verify_contract(name, collaborator, block)
-    end
-
-    def self.summary
-      @ledger.summary
-    end
-  end
+  @@ledger = Ledger.new
 
   def self.prepare_double(name, block = Proc.new)
-    LedgerWrapper.prepare_double(name, block)
+    @@ledger.prepare_double(name, block)
   end
 
   def self.verify_contract(name, collaborator, block = Proc.new )
-    LedgerWrapper.verify_contract(name, collaborator, block)
+    @@ledger.verify_contract(name, collaborator, block)
   end
 
   def self.summary
-    LedgerWrapper.summary
+    @@ledger.summary
   end
 
 end
